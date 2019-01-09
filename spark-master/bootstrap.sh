ce2ded54db1104ce03dd781f368be0e966a066d0
Yue Shi <october_shi_1985@hotmail.com>
2019-01-09 19:38:43 +0800

    spark support

--- spark-master/bootstrap.sh
+++ spark-master/bootstrap.sh
@@ -4,7 +4,8 @@
 # sed s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
 
 /usr/sbin/sshd -D &
-$HADOOP_HOME/bin/hdfs namenode &
-$HADOOP_HOME/bin/yarn resourcemanager &
+$HADOOP_HOME/sbin/start-dfs.sh &
+$HADOOP_HOME/sbin/start-yarn.sh &
 $HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver &
+$SPARK_HOME/sbin/start-all.sh &
 bash
--- spark-slave/bootstrap.sh
+++ spark-slave/bootstrap.sh
@@ -4,7 +4,4 @@
 # sed s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
 
 /usr/sbin/sshd -D &
-$HADOOP_HOME/bin/hdfs datanode &
-$HADOOP_HOME/bin/yarn nodemanager &
-$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver &
 bash
