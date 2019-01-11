87db6b974989a592d8095a7531a85030b50c190c
lastoctober <shiy@ipharmacare.net>
2019-01-11 12:51:43 +0800

    start cluster in swarm

--- spark-slave/bootstrap.sh
+++ spark-slave/bootstrap.sh
@@ -5,3 +5,7 @@
 
 /usr/sbin/sshd -D &
 bash
+#$HADOOP_HOME/bin/hdfs datanode &
+#$HADOOP_HOME/bin/yarn nodemanager &
+#$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver &
+#$SPARK_HOME/sbin/start-all.sh &
